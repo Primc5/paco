a68a7d56c3759b72d404fbfe4b6402dec7f413cb
Primc5 <pablocruzmendez@gmail.com>
2018-04-03 14:58:18 +0200

    botones y mas

--- Prueba/ViewController.swift
+++ Prueba/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblPrueba:UILabel?
+    @IBOutlet var txtUser:UITextField?
+    @IBOutlet var txtPass:UITextField?
+    @IBOutlet var bttlogin:UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblPrueba?.text = "hola"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func eventologin(){
+        print("hey"+(txtUser?.text)!)
+        if txtUser?.text == "Yony" {
+            print("heylo")
+            self.performSegue(withIdentifier: "trlogin", sender: self)
+        }
+    }
 
 }
+
 
